d38dea0eeaac746e3cd0c4c8f14d750bad0fae96
Cheryl Chin <cherylchq@gmail.com>
2025-03-10 04:05:36 +0800

    Remove unnecessary icon

--- TruthGuard/src/components/FactCheckForm.jsx
+++ TruthGuard/src/components/FactCheckForm.jsx
@@ -54,7 +54,7 @@ function FactCheckForm({ onSubmit, isLoading }) {
               placeholder="Paste an article, claim, or statement to verify"
               disabled={isLoading}
             ></textarea>
-            
+            {/*
             <button
               type="button"
               onClick={triggerFileInput}
@@ -66,7 +66,7 @@ function FactCheckForm({ onSubmit, isLoading }) {
                 <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
               </svg>
             </button>
-            
+            */}
             <input
               ref={fileInputRef}
               type="file"
